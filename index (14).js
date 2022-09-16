668cac427676e5856afe8554b896571b17b86ae8
Syed Obaid Ullah Hashmi <79754399+oobaidhashmi089@users.noreply.github.com>
2022-09-16 22:49:37 +0500

    countApplesAndOranges hackerRank
      countApplesAndOranges hackerRank

---
+++ index (14).js
@@ -0,0 +1,45 @@
+let apples=[2,3,-4] 
+let oranges=[3,-2,-4] 
+let a=4
+let b=12
+let s=7
+let t=10
+function countApplesAndOranges(s, t, a, b, apples, oranges) 
+{
+    // Write your code here
+  let sumA = 0; 
+  let sumo = 0;
+  let o=0
+  let ap=0
+ 
+  for (let i=0 ; i<apples.length;i++)
+  {
+    let sumA= apples[i]+a;
+      if(sumA >=s && sumA<=t)
+    {
+      ap =ap+1
+      
+    }
+    
+    
+  }
+  
+    for (let i=0 ; i<apples.length;i++)
+  {
+    let sumo= oranges[i]+b;
+    if(sumo >=s && sumo<=t)
+    {
+      o =o+1
+      
+    }
+    
+    
+  }
+ console.log(ap)
+ console.log(o)
+
+}
+
+let rs = countApplesAndOranges(s,t,a,b,apples,oranges)
+// expected output =2  
+console.log(rs)  //
